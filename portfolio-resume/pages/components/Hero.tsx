515403889df8f617880050c224b0a718d969612c
gabrielb5 <104873216+gabrielb5@users.noreply.github.com>
2022-11-15 09:32:20 -0700

    working

--- portfolio-resume/pages/components/Hero.tsx
+++ portfolio-resume/pages/components/Hero.tsx
@@ -5,8 +5,6 @@ import Image from 'next/image'
 import Link from 'next/link'
 import Golden from './golden.jpg'
 
-import { motion } from "framer-motion"
-
 type Props = {};
 
 
@@ -26,25 +24,11 @@ function Hero({} : Props) {
    <div className='h-screen flex flex-col space-y-8 items-center justify-center overflow-hidden text-center'>
     <BackgroundCircles />
     <div className='z-20'>
-    <motion.img src='https://media-exp1.licdn.com/dms/image/C4E03AQENmSp952_JEQ/profile-displayphoto-shrink_400_400/0/1661457257888?e=1671667200&v=beta&t=ekmY7V3IwOu_51Reu6f8NwnzmHoUtICYr6d28r8EZD8'
-
-className='-mb-20 sm:mb-10 md:mb-0 mt-[100px] flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-full  md:w-95 md:h-95 xl:w-600 xl:h-600 xl:mt-[150px] '
-
-initial={{
-    x:-200,
-    opacity: 0 
-}}
-transition={{
-    duration: 1.2,
-
-}}
-whileInView={{
-    x:0,
-    opacity: 1
-}}
-viewport={{ once: true}}>
-
-</motion.img>
+      <Image  width={150}
+          className='rounded-full relative mx-auto object-cover'
+          height={150}
+          src={Golden}
+          alt="Profile" />
     
         <h2 className='text-[10px] uppercase text-gray-500 pb-2 tracking-[15px]'> {role} </h2>
         <h1 className='text-3xl lg:text-5xl font-semibold scroll-px-10'>
